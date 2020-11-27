f01556a16e1581921a853d78c2342107b42ab3a7
Peter van Dijk <peter.van.dijk@bynder.com>
2020-11-27 18:16:24 +0100

    docker porting ok

--- scripts/up.sh
+++ scripts/up.sh
@@ -4,6 +4,6 @@ export DIR_DATA_PATH="$PWD"
 
 export CONTAINER_COMMAND="npm run start:dev"
 export CONTAINER_SCALE="1"
-export CONTAINER_PORT="3000"
+export CONTAINER_PORT="7070"
 
 docker-compose up --build
