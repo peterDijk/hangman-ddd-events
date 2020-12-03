f4b0be3c0ee444e35a84ad22e248938525d4276c
Peter van Dijk <peter.van.dijk@bynder.com>
2020-12-03 12:07:46 +0100

    swagger available at /api

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,13 @@
-import { Controller, Get, HttpCode, Res } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { AppService } from './app.service';
 
 @Controller('health')
+@ApiTags('Health check')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @ApiResponse({ status: 200, description: 'Perform API health check' })
   @Get()
   getHealth(): Record<string, unknown> {
     return this.appService.getHealth();
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { Logger } from '@nestjs/common';
+
 import { AppModule } from './app.module';
+import { config } from '../config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.setGlobalPrefix(config.PREFIX);
+  const documentOptions = new DocumentBuilder()
+    .setTitle(config.TITLE)
+    .setDescription(config.DESCRIPTION)
+    .setVersion(config.VERSION)
+    .build();
+  const document = SwaggerModule.createDocument(app, documentOptions);
+  SwaggerModule.setup(config.API_EXPLORER_PATH, app, document);
+
+  await app.listen(config.PORT, config.HOST);
+  Logger.log(`Server listening on port ${config.PORT}`, 'Bootstrap');
 }
 bootstrap();
