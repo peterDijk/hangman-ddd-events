c922499074d2ab4a8630cb19d443fd31c5daf0c1
Peter van Dijk <peter.van.dijk@bynder.com>
2020-12-04 15:08:08 +0100

    correct docker ports setup

--- scripts/build-up.sh
+++ scripts/build-up.sh
@@ -4,6 +4,7 @@ export DIR_DATA_PATH="$PWD"
 
 export CONTAINER_COMMAND="npm run start:dev"
 export CONTAINER_SCALE="1"
-export CONTAINER_PORT="7070"
+export APP_PORT="7070"
+export CONTAINER_PORT="3000"
 
 docker-compose up --build
