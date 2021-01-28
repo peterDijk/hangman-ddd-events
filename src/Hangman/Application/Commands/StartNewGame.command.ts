1e8f7507664bf8f01a0817e830ccc41a40cc6535
Peter van Dijk <peter.van.dijk@bynder.com>
2021-01-28 20:46:22 +0100

    more implicit

--- src/Hangman/Application/Commands/StartNewGame.command.ts
+++ src/Hangman/Application/Commands/StartNewGame.command.ts
@@ -2,5 +2,20 @@ import { ICommand } from '@nestjs/cqrs';
 import { GameDto } from 'src/Hangman/Domain/AggregateRoot/GameDto';
 
 export class StartNewGameCommand implements ICommand {
-  constructor(public readonly gameDto: GameDto) {}
+  gameDto = {} as GameDto;
+
+  constructor({ gameId, playerId, wordToGuess, maxGuesses }: GameDto) {
+    if (
+      !gameId &&
+      !playerId &&
+      !wordToGuess &&
+      wordToGuess !== '' &&
+      maxGuesses &&
+      maxGuesses > 0
+    ) {
+      throw new Error('u oh');
+    }
+
+    this.gameDto = { gameId, playerId, wordToGuess, maxGuesses };
+  }
 }
