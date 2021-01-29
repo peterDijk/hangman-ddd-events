4b01c4287eeb7a54f69617959da29f322c8b985d
Peter van Dijk <peter.van.dijk@bynder.com>
2021-01-29 17:13:54 +0100

    cleaned up command

--- src/Hangman/Application/Commands/StartNewGame.command.ts
+++ src/Hangman/Application/Commands/StartNewGame.command.ts
@@ -1,65 +1,10 @@
 import { ICommand } from '@nestjs/cqrs';
-import {
-  IsString,
-  validate,
-  validateOrReject,
-  IsNumber,
-  MinLength,
-  Min,
-} from 'class-validator';
-
-import { InvalidCommandException } from 'src/Hangman/Exceptions';
-import { Logger } from '@nestjs/common';
 
 export class StartNewGameCommand implements ICommand {
-  @IsString()
-  @MinLength(4)
-  gameId: string;
-
-  @IsString()
-  @MinLength(2)
-  playerId: string;
-
-  @IsString()
-  wordToGuess: string;
-
-  @IsNumber()
-  @Min(1)
-  maxGuesses: number;
-
   constructor(
-    gameId: string,
-    playerId: string,
-    wordToGuess: string,
-    maxGuesses: number,
-  ) {
-    this.gameId = gameId;
-    this.playerId = playerId;
-    this.wordToGuess = wordToGuess;
-    this.maxGuesses = maxGuesses;
-
-    // TODO: make class-validator work (= promise, so cant wait for it in constructor)
-    // validateOrReject(this).catch((err) => {
-    //   Logger.log('validation error', JSON.stringify(err));
-    //   throw new InvalidCommandException('InvalidCommandException');
-    // });
-
-    // if (
-    //   !gameId ||
-    //   gameId === '' ||
-    //   !playerId ||
-    //   playerId === '' ||
-    //   !wordToGuess ||
-    //   wordToGuess === '' ||
-    //   !maxGuesses ||
-    //   maxGuesses < 1
-    // ) {
-    //   Logger.log(
-    //     { gameId, playerId, wordToGuess, maxGuesses },
-    //     'validation error',
-    //   );
-
-    //   throw new InvalidCommandException();
-    // }
-  }
+    public gameId: string,
+    public playerId: string,
+    public wordToGuess: string,
+    public maxGuesses: number,
+  ) {}
 }
