c383d8f13b50cb771a7eaffbb83378388dd59e41
Peter van Dijk <peter.van.dijk@bynder.com>
2021-04-24 20:17:09 +0200

    event handler writes

--- src/Hangman/Domain/EventHandlers/NewGameStarted.handler.ts
+++ src/Hangman/Domain/EventHandlers/NewGameStarted.handler.ts
@@ -1,14 +1,29 @@
 import { EventsHandler, IEventHandler } from '@nestjs/cqrs';
 import { NewGameStartedEvent } from '../Events/NewGameStarted.event';
 import { Logger } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Game } from '../../ReadModels/game.entity';
+import { Repository } from 'typeorm';
 
 @EventsHandler(NewGameStartedEvent)
 export class NewGameStartedEventHandler
   implements IEventHandler<NewGameStartedEvent> {
+  constructor(
+    @InjectRepository(Game)
+    private gamesRepository: Repository<Game>,
+  ) {}
   private readonly logger = new Logger(NewGameStartedEventHandler.name);
 
-  handle(event: NewGameStartedEvent) {
+  async handle(event: NewGameStartedEvent) {
     // write to projection database?
     // this.logger.log(event);
+    const newGame = await this.gamesRepository.create({
+      gameId: event.gameId,
+      playerId: event.playerId,
+      playerName: '',
+      wordToGuess: event.wordToGuess,
+      maxGuesses: event.maxGuesses,
+    });
+    await this.gamesRepository.save(newGame);
   }
 }
