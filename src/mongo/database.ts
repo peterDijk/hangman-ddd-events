1ad6a813b20c3a151480eddfd60382f6b0a32575
Peter van Dijk <peter.van.dijk@bynder.com>
2021-12-06 20:26:38 +0100

    clean up

--- src/mongo/database.ts
+++ src/mongo/database.ts
@@ -2,9 +2,6 @@ import { config } from '../../config';
 import * as Mongoose from 'mongoose';
 let database: Mongoose.Connection;
 export const connect = () => {
-  // add your own uri below
-  // const uri =
-  //   'mongodb://root:example@store-state.db:27017/?retryWrites=true&w=majority';
   const uri = `${config.STORE_STATE_SETTINGS.type}://${config.STORE_STATE_SETTINGS.credentials.username}:${config.STORE_STATE_SETTINGS.credentials.password}@${config.STORE_STATE_SETTINGS.hostname}:${config.STORE_STATE_SETTINGS.port}/?retryWrites=true&w=majority`;
   if (database) {
     return;
--- src/mongo/store-state.model.ts
+++ src/mongo/store-state.model.ts
@@ -2,8 +2,6 @@ import { model } from 'mongoose';
 import { IStoreStateDocument, IStoreStateModel } from './store-state.types';
 import StoreStateSchema from './store-state.schema';
 
-// const User = model<IUser, UserModel>('User', schema);
-
 export const StoreStateModel = model<IStoreStateDocument, IStoreStateModel>(
   'store-state',
   StoreStateSchema,
