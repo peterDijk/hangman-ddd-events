00ca52540eb574b1c6ee0f8991d7e93b23ba6989
Peter van Dijk <peter.van.dijk@bynder.com>
2021-12-10 22:44:43 +0100

    subscribeToCatchUpSubscriptions

--- src/Hangman/Domain/Repositories/GamesRepository.ts
+++ src/Hangman/Domain/Repositories/GamesRepository.ts
@@ -1,10 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { EventStore } from '@juicycleff/nestjs-event-store';
+import { Injectable, Logger } from '@nestjs/common';
 import { GameDto } from '../../Infrastructure/Dto/Game.dto';
 import { Game } from '../AggregateRoot/Game.aggregate';
 
 @Injectable()
 export class GamesRepository {
+  constructor(private readonly eventStore: EventStore) {}
+  private logger = new Logger(GamesRepository.name);
+
+  private findOneById(id: string) {
+    const startingPosition = 0;
+    const game = this.eventStore.subscribeToCatchupSubscription(
+      `game-${id}`,
+      true,
+      startingPosition,
+    );
+    return game;
+  }
   async startNewGame(data: GameDto, uuid: string) {
+    const testGame = this.findOneById(uuid);
+    this.logger.log(
+      `eventstoresubscribeToCatchupSubscription : ${JSON.stringify(testGame)}`,
+    );
+
     const game = new Game(data, uuid);
     await game.startNewGame();
     return game;
