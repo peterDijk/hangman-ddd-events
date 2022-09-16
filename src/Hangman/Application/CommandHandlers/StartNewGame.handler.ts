c3da2c7e4caed3fee1c951e8cd70e7d34d0ec67c
Peter van Dijk <peter.van.dijk@bynder.com>
2022-09-16 16:32:47 +0200

    logs

--- src/Hangman/Application/CommandHandlers/StartNewGame.handler.ts
+++ src/Hangman/Application/CommandHandlers/StartNewGame.handler.ts
@@ -7,17 +7,21 @@ import { Game } from '../../Domain/AggregateRoot/Game.aggregate';
 
 @CommandHandler(StartNewGameCommand)
 export class StartNewGameCommandHandler
-  implements ICommandHandler<StartNewGameCommand> {
+  implements ICommandHandler<StartNewGameCommand>
+{
   private readonly logger = new Logger(StartNewGameCommandHandler.name);
 
   constructor(private publisher: StoreEventPublisher) {}
 
   async execute({ data, uuid }: StartNewGameCommand) {
+    this.logger.log(`executing command`);
     const aggregate = new Game(uuid);
     await aggregate.startNewGame(data);
 
+    this.logger.log('publishing');
     const game = this.publisher.mergeObjectContext(aggregate);
-
+    this.logger.log(game);
     game.commit();
+    this.logger.debug('committed?');
   }
 }
