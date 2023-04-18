3f52d7bf478e6e879911f02715a564293b03aeef
Peter van Dijk <peter.van.dijk@bynder.com>
2023-04-18 15:30:33 +0200

    writing to and reading from redis, rediscommander not connecting = docker network issue

--- src/infrastructure/modules/app.module.ts
+++ src/infrastructure/modules/app.module.ts
@@ -1,3 +1,4 @@
+import { redisStore } from 'cache-manager-redis-yet';
 import { CacheModule, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
@@ -17,10 +18,22 @@ import { MongoPositionStore } from '../../mongo/mongo-eventstore-adapter';
 export const mongoDbUri = `${config.STORE_STATE_SETTINGS.type}://${config.STORE_STATE_SETTINGS.credentials.username}:${config.STORE_STATE_SETTINGS.credentials.password}@${config.STORE_STATE_SETTINGS.hostname}:${config.STORE_STATE_SETTINGS.port}`;
 @Module({
   imports: [
-    CacheModule.register({
-      ttl: 3600 * 60 * 24,
-      // max: 1000 * 1000 * 1000 * 1000,
+    // CacheModule.register({
+    //   ttl: 3600 * 60 * 24,
+    //   // max: 1000 * 1000 * 1000 * 1000,
+    //   isGlobal: true,
+    // }),
+    CacheModule.registerAsync<any>({
       isGlobal: true,
+      useFactory: async () => {
+        const store = await redisStore({
+          url: 'redis://redis:6379',
+          ttl: 0,
+        });
+        return {
+          store: () => store,
+        };
+      },
     }),
     AuthModule,
     GraphQLModule.forRoot<ApolloDriverConfig>({
